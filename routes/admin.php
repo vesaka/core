8e898be871b20ceea72f7a1cc433def0c2623c7d
Veselin <vesakabgr@gmail.com>
2023-05-29 06:58:54 +0300

    added website and skill resources

--- routes/admin.php
+++ routes/admin.php
@@ -1,15 +1,17 @@
 <?php
+
 //use Vesaka\Core\Http\Controllers\Admin\DashboardController;
 Route::namespace('Admin')
         ->middleware('auth')
-        ->group(function() {
+        ->group(function () {
             Route::get('model/items', 'ModelController@datatable')->name('model.items');
-            
+            Route::get('list/items', 'ModelController@paginate')->name('paginate.items');
+
             Route::resource('user', 'UserController');
             Route::resource('image', 'ImageController');
+            Route::resource('website', 'WebsiteController');
+            Route::resource('skill', 'SkillController');
             Route::resource('model', 'ModelController');
             Route::resource('category', 'CategoryController');
-            Route::get('dashboard', 'DashboardController@dashboard');
-            
-            
-        });
+            Route::get('dashboard', 'DashboardController@dashboard')->name('dashboard');
+});
