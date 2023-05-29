ee39051543372c616ffdc6c30fc1731e75524ec7
Veselin <vesakabgr@gmail.com>
2023-05-29 06:57:32 +0300

    update admin navigation links

--- config/navbar.php
+++ config/navbar.php
@@ -4,8 +4,8 @@ return [
     'menu' => [
         [
             'label' => 'Dashboard',
-            'name' => 'home',
-            'route' => 'dashboard',
+            'name' => 'admin::dashboard',
+            'route' => 'admin::dashboard',
             'icon' => ''
         ],
         [
@@ -24,6 +24,44 @@ return [
                     'label' => 'Images',
                     'icon' => 'add',
                     'route' => 'admin::image.index',
+                ],
+            ]
+        ],
+        [
+            'label' => 'Websites',
+            'name' => 'websites',
+            'icon' => 'website',
+            'children' => [
+                [
+                    'name' => 'new-website',
+                    'label' => 'New Website',
+                    'icon' => 'add',
+                    'route' => 'admin::website.create',
+                ],
+                [
+                    'name' => 'websites',
+                    'label' => 'Websites',
+                    'icon' => 'add',
+                    'route' => 'admin::website.index',
+                ],
+            ]
+        ],
+        [
+            'label' => 'Skills',
+            'name' => 'skills',
+            'icon' => 'skill',
+            'children' => [
+                [
+                    'name' => 'new-skill',
+                    'label' => 'New Skill',
+                    'icon' => 'add',
+                    'route' => 'admin::skill.create',
+                ],
+                [
+                    'name' => 'skills',
+                    'label' => 'Skills',
+                    'icon' => 'add',
+                    'route' => 'admin::skill.index',
                 ],
             ]
         ]
