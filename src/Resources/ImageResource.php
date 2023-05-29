3399f116fa6f228747a0dac93f19e6502c883670
Veselin <vesakabgr@gmail.com>
2023-05-29 06:44:24 +0300

    added Resources folder

---
+++ src/Resources/ImageResource.php
@@ -0,0 +1,12 @@
+<?php
+namespace Vesaka\Core\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+/**
+ * Description of ImageResource
+ *
+ * @author vesak
+ */
+class ImageResource extends JsonResource {
+    //put your code here
+}
---
+++ src/Resources/ModelResource.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Vesaka\Core\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+/**
+ * Description of ModelResource
+ *
+ * @author vesak
+ */
+class ModelResource extends JsonResource {
+    //put your code here
+}
