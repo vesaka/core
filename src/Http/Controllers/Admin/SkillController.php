43153c5392fba5310dc62c0d5fec320cee62ba6f
Veselin <vesakabgr@gmail.com>
2023-05-29 06:46:12 +0300

    added SkillsController

---
+++ src/Http/Controllers/Admin/SkillController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Vesaka\Core\Http\Controllers\Admin;
+
+/**
+ * Description of SkillController
+ *
+ * @author vesak
+ */
+class SkillController extends ModelController {
+    //put your code here
+}
