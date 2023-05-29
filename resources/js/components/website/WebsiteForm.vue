6186553ee39d4863c5c0c519d6c5722e97bb74cf
Veselin <vesakabgr@gmail.com>
2023-05-29 06:50:04 +0300

    added website component

---
+++ resources/js/components/website/WebsiteForm.vue
@@ -0,0 +1,8 @@
+<template>
+    <ModelForm type="website" uploader="filepond">
+        
+    </ModelForm>
+</template>
+<script setup>
+    import ModelForm from '@core/components/model/ModelForm.vue'
+</script>
