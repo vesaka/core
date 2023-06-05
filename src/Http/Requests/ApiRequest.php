0168f66c14e0844cd272eea97ba6eb4a2bb53f8a
Veselin <vesakabgr@gmail.com>
2023-06-05 19:50:08 +0300

    ApiRequest class to hanle typed inputs

--- src/Http/Requests/ApiRequest.php
+++ src/Http/Requests/ApiRequest.php
@@ -14,6 +14,8 @@ class ApiRequest extends FormRequest {
     protected $rules;
 
     protected $messages = [];
+    
+    const TYPES = ['string', 'numeric', 'array', 'file'];
 
     public function authorize() {
         return true;
@@ -23,27 +25,33 @@ class ApiRequest extends FormRequest {
         $messages = [];
         $rules = $this->rules();
         foreach ($rules as $key => $rule) {
-            if (is_string($rule)) {
-                $list = explode('|', $rule);
-            } else {
-                $list = $rule;
+            $suffix = '';
+            $list = is_string($rule) ? explode('|', $rule) : $rule;
+            
+            $matchedTypes = array_values(array_filter(self::TYPES, function($type) use ($list) {
+                return in_array($type, $list);
+            }));
+
+            if (isset($matchedTypes[0])) {
+                $suffix = '.' . $matchedTypes[0];
             }
             foreach ($list as $item) {
                 if ($item instanceof Rule) {
                     $classname = get_class($item);
                     $class = substr($classname, strrpos($classname, '\\') + 1);
-                    $messages[$key.'.'.$class]['rule'] = $item->message();
+                    $messages[$key.'.'.$class . $suffix]['rule'] = $item->message();
                 } else {
                     $name = explode(':', $item, 2);
-                    $messages[$key.'.'.$name[0]]['rule'] = $name[0];
-                    if (! isset($name[1])) {
+                    $messageKey = $key . '.' . $name[0] . $suffix;
+                    $messages[$messageKey]['rule'] = $name[0];
+                    if (!isset($name[1])) {
                         continue;
                     }
 
                     if (method_exists($this, $name[0])) {
-                        $messages[$key.'.'.$name[0]]['value'] = $this->{$name[0]}($name[1]);
+                        $messages[$messageKey]['value'] = $this->{$name[0]}($name[1]);
                     } else {
-                        $messages[$key.'.'.$name[0]]['value'] = $name[1];
+                        $messages[$messageKey]['value'] = $name[1];
                     }
                 }
             }
